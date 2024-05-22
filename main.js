ac7613fbb4562d2e8df0733843094729d833a4f9
Victor Villazón <victorzn23@gmail.com>
2024-05-21 21:01:59 -0500

    agregando javascript al color picker

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hola JS!')
+// console.log('Hola JS!')
+
+const inputColor = document.querySelector('#color')
+
+// console.log(inputColor)
+// console.log(inputColor.placeholder)
+
+inputColor.addEventListener('input', function (event) {
+  console.log(event.target.value)
+  //  style="background-color: blue;"
+  document.body.style.backgroundColor = event.target.value
+})
